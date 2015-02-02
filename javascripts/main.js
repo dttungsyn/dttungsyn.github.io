61388bd62d740f0110027af1912d1b8a149ae941
dttung1412 <dttung1412@gmail.com>
2015-02-03 03:34:11 +0900

    brief profile

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,18 @@
 console.log('This would be the main JS file.');
 $(document).ready(function(){
 	$(".dial").knob();
-	new WOW().init();
+	new WOW({
+        mobile: false
+    }).init();
+	
+	$.backstretch(["images/beautiful-white-flowers.jpg"], {
+        duration: 3800,
+        fade: 1500
+    });
+	
+	$('.brief').affix({
+	  offset: {
+	    top: $(window).height()
+	  }
+	})
 });
